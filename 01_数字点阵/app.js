03c05f57100b19dd1aed6d9c7b7f0f3fa356e01f
SnailSword <saniac@163.com>
2016-06-23 15:11:51 +0800

    刷新可变时间

--- 01_数字点阵/app.js
+++ 01_数字点阵/app.js
@@ -11,6 +11,16 @@ var DIGIT_SIZE = [digit[0].length,digit[0][0].length];
 var COLON_SIZE = [digit[10].length,digit[10][0].length];
 var DIGIT_SPACE = 30;
 
+var END_TIME = new Date(2016,5,25,18,47,10);
+var curShowTimeSeconds = getCurShowTimeSeconds();
+
+function getCurShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = END_TIME.getTime() - curTime.getTime();
+    ret = Math.round(ret/1000);
+    return ret;
+}
+
 window.onload = function () {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
@@ -19,21 +29,23 @@ window.onload = function () {
     canvas.height = WINDOW_HEIGHT;
 
     render(context);
+    // console.log(END_TIME.getTime());
 }
 
 function render(cxt) {
-
-    var hours = 12;
-    var mintutes = 34;
-    var seconds = 56;
+    // console.log(curShowTimeSeconds);
+    var hours = parseInt(curShowTimeSeconds/3600);
+    // console.log(hours);
+    var minutes = parseInt(curShowTimeSeconds%3600/60);
+    var seconds = curShowTimeSeconds%60;
 
 
     renderDigit(MARGIN_LEFT, MARGIN_TOP,parseInt(hours/10),cxt);
     renderDigit(MARGIN_LEFT+ DIGIT_SIZE[1]*2*(RADIUS+SPACE)+DIGIT_SPACE, MARGIN_TOP,hours%10,cxt);
     renderDigit(MARGIN_LEFT+ (DIGIT_SIZE[1]*2)*2*(RADIUS+SPACE)+DIGIT_SPACE*2, MARGIN_TOP,10,cxt);
 
-    renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]+DIGIT_SIZE[1]*2)*2*(RADIUS+SPACE)+DIGIT_SPACE*3, MARGIN_TOP,parseInt(mintutes/10),cxt);
-    renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]+DIGIT_SIZE[1]*3)*2*(RADIUS+SPACE)+DIGIT_SPACE*4, MARGIN_TOP,parseInt(mintutes%10),cxt);
+    renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]+DIGIT_SIZE[1]*2)*2*(RADIUS+SPACE)+DIGIT_SPACE*3, MARGIN_TOP,parseInt(minutes/10),cxt);
+    renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]+DIGIT_SIZE[1]*3)*2*(RADIUS+SPACE)+DIGIT_SPACE*4, MARGIN_TOP,parseInt(minutes%10),cxt);
     renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]+DIGIT_SIZE[1]*4)*2*(RADIUS+SPACE)+DIGIT_SPACE*5, MARGIN_TOP,10,cxt);
 
     renderDigit(MARGIN_LEFT+ (COLON_SIZE[1]*2+DIGIT_SIZE[1]*4)*2*(RADIUS+SPACE)+DIGIT_SPACE*6, MARGIN_TOP,parseInt(seconds/10),cxt);
